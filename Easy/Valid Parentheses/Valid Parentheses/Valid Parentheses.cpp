69bfb6e8820b0246a0f5c2387d8f22db81e84acc
Alexei <antipovalexei23@gmail.com>
2025-06-01 13:38:08 +0300

    Add files via upload

---
+++ Easy/Valid Parentheses/Valid Parentheses/Valid Parentheses.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> bracketStack;
+        unordered_map<char, char> bracketPairs = { {')', '('}, {']', '['}, {'}', '{'} };
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s[i];
+            if (bracketPairs.find(currentChar) == bracketPairs.end()) {
+                bracketStack.push(currentChar);
+            }
+            else if (!bracketStack.empty() && bracketPairs[currentChar] == bracketStack.top()) {
+                bracketStack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return bracketStack.empty();
+    }
+};
+
+int main() {
+    Solution solution;
+    string testCases[] = { "()", "()[]{}", "(]", "([])" };
+    for (int i = 0; i < sizeof(testCases) / sizeof(testCases[0]); i++) {
+        cout << "String: " << testCases[i];
+        if (solution.isValid(testCases[i])) {
+            cout << " is valid\n";
+        }
+        else {
+            cout << " is not valid.\n";
+        }
+    }
+    return 0;
+}
