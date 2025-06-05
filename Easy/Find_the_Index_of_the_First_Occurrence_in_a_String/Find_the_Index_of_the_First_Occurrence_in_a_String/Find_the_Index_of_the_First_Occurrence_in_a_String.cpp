4ca9f1c57d10cf36a9beaf73a5fdf1dbd19ea988
Alexei <antipovalexei23@gmail.com>
2025-06-05 18:06:36 +0300

    Add files via upload

---
+++ Easy/Find_the_Index_of_the_First_Occurrence_in_a_String/Find_the_Index_of_the_First_Occurrence_in_a_String/Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+		int repeat = 0;
+		for (int i = 0; i < haystack.length(); i++)
+		{
+			if (haystack[i] == needle[0])
+			{
+				for (int j = 0, k = i; j < needle.length() && k < haystack.length(); j++, k++)
+				{
+					if (haystack[k] == needle[j])
+					{
+						repeat++;
+					}
+				}
+				if (repeat == needle.length())
+				{
+					return i;
+				}
+				else
+				{
+					repeat = 0;
+				}
+			}
+		}
+		return -1;
+    }
+};
+
+int main()
+{
+	Solution sol;
+	sol.strStr("sldsad", "sad");
+}
